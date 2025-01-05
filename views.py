65029cf0c790ddbe9cc96a7ad90fe63b60b1af4c
No.eito <s1f102202662@iniad.org>
2025-01-05 23:27:23 +0900

    accountテーブルの定義を変更

--- views.py
+++ views.py
@@ -36,7 +36,7 @@ db = SQLAlchemy(my_app)
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.Text, nullable=False)
     
     def __repr__(self):
         return f'<Account {self.username}>'
